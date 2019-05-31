f2770d36977e44b83d83243185a6bf6adf73c9e5
liyang.wang <liyang.wang@21chinamall.com>
2019-05-31 10:40:56 +0800

    内容变更： 1、新增 第一个界面。

---
+++ app/src/main/java/com/axzl/mobile/refueling/di/component/MainComponent.java
@@ -0,0 +1,41 @@
+package com.axzl.mobile.refueling.di.component;
+
+import dagger.BindsInstance;
+import dagger.Component;
+
+import com.jess.arms.di.component.AppComponent;
+
+import com.axzl.mobile.refueling.di.module.MainModule;
+import com.axzl.mobile.refueling.mvp.contract.MainContract;
+
+import com.jess.arms.di.scope.ActivityScope;
+import com.axzl.mobile.refueling.mvp.ui.activity.MainActivity;
+
+
+/**
+ * ================================================
+ * Description:
+ * <p>
+ * Created by MVPArmsTemplate on 05/31/2019 10:34
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArms">Star me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArms/wiki">See me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArmsTemplate">模版请保持更新</a>
+ * ================================================
+ */
+@ActivityScope
+@Component(modules = MainModule.class, dependencies = AppComponent.class)
+public interface MainComponent {
+    void inject(MainActivity activity);
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        MainComponent.Builder view(MainContract.View view);
+
+        MainComponent.Builder appComponent(AppComponent appComponent);
+
+        MainComponent build();
+    }
+}
