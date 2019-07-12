e8b7ad4cc7bf306c8ee520dc60c6ef36805c69b3
liyang.wang <liyang.wang@21chinamall.com>
2019-07-12 17:32:17 +0800

    内容变更： 1、新增高德地图功能。

--- app/src/main/java/com/axzl/mobile/refueling/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/com/axzl/mobile/refueling/mvp/ui/activity/MainActivity.java
@@ -2,11 +2,17 @@ package com.axzl.mobile.refueling.mvp.ui.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.MyLocationStyle;
 import com.axzl.mobile.refueling.R;
 import com.axzl.mobile.refueling.di.component.DaggerMainComponent;
 import com.axzl.mobile.refueling.mvp.contract.MainContract;
@@ -16,6 +22,7 @@ import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 import static com.jess.arms.utils.Preconditions.checkNotNull;
@@ -27,6 +34,32 @@ import static com.jess.arms.utils.Preconditions.checkNotNull;
  * ================================================
  */
 public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.View {
+    @BindView(R.id.mavi_mapview)
+    MapView mMapView;                                                                               // 地图
+
+    // 地图控制器对象
+    private AMap mAmap;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
 
     /**
      * 关闭滑动返回
@@ -47,13 +80,56 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
     public int initView(@Nullable Bundle savedInstanceState) {
         return R.layout.activity_main;
     }
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
+        // 创建地图
+        initMapView(savedInstanceState);
+        setLocation();
+        // 初始化
         mPresenter.initPresenter();
+    }
+
+    /**
+     * 初始化地图
+     */
+    private void initMapView(@Nullable Bundle savedInstanceState) {
+        mMapView.onCreate(savedInstanceState);
+        mAmap = mMapView.getMap();
+    }
+
+    /**
+     * 设置当前位置
+     */
+    private void setLocation() {
+        // 初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        // 连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);
+        // 设置圆形的边框颜色
+        myLocationStyle.strokeColor(Color.argb(0, 0, 0, 0));
+        // 设置圆形的填充颜色
+        myLocationStyle.radiusFillColor(Color.argb(0, 0, 0, 0));
+        // 设置中心点图标
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.mipmap.skin3));
+        // 设置定位蓝点的Style
+        mAmap.setMyLocationStyle(myLocationStyle);
+        // 设置隐藏缩放按钮
+        mAmap.getUiSettings().setZoomControlsEnabled(false);
+        // 设置希望展示的地图缩放级别
+        mAmap.moveCamera(CameraUpdateFactory.zoomTo(17));
+        // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        mAmap.setMyLocationEnabled(true);
     }
 
     @OnClick({
